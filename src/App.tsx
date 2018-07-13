f6c0c9e25bb48c49bddec494e159026aea0df0a5
Patrick Nolin <patrick.nolin95@gmail.com>
2018-07-12 21:35:38 -0400

    Lifted state up.
    - File selector is only responsible of the file selecting.
    - App decides to show file selector or ranker.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,64 @@
 import * as React from 'react';
 
+import RankerRenderer from './components/ranker/rankerRenderer';
 import SelectFileRenderer from './components/select-file/selectFileRenderer';
 
-class App extends React.Component {
+interface IAppState {
+  file: File | null;
+  items: string[];
+}
+
+class App extends React.Component<{}, IAppState> {
+  constructor(props: {}) {
+    super(props);
+    this.onFileChanged = this.onFileChanged.bind(this);
+    this.rankClick = this.rankClick.bind(this);
+    this.createItemArray = this.createItemArray.bind(this);
+    this.state = { file: null, items: Array<string>() };
+  }
+
   public render() {
-    return (
-      <div>
-        <SelectFileRenderer />
-      </div>
+    const selectFileRenderer = (
+      <SelectFileRenderer onFileChanged={this.onFileChanged} />
     );
+    const rankButton = <button onClick={this.rankClick}>Rank</button>;
+    const rankerComponent = <RankerRenderer items={this.state.items} />;
+
+    if (this.state.items.length === 0 && this.state.file === null) {
+      return <div>{selectFileRenderer}</div>;
+    } else if (this.state.items.length === 0) {
+      return (
+        <div>
+          {selectFileRenderer}
+          {rankButton}
+        </div>
+      );
+    } else {
+      return <div>{rankerComponent}</div>;
+    }
+  }
+
+  private onFileChanged(file: File | null) {
+    this.setState({ file });
+  }
+
+  private rankClick() {
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      const text = fileReader.result;
+      this.createItemArray(text as string);
+    };
+
+    fileReader.readAsText(this.state.file as File);
+  }
+
+  private createItemArray(fileContent: string) {
+    let lines = fileContent.split('\r');
+    lines = lines.map(line => line.replace(/\r?\n|\r/g, ''));
+    let items = this.state.items.splice(0);
+    items = items.concat(lines.splice(0));
+    this.setState({ items });
   }
 }
 
--- src/components/select-file/selectFileRenderer.tsx
+++ src/components/select-file/selectFileRenderer.tsx
@@ -1,75 +1,35 @@
 import * as React from 'react';
 
-import RankerRenderer from '../ranker/rankerRenderer';
-
-interface ISelectFileState {
-  selectedFile: File | null;
-  items: string[];
+interface ISelectFileProps {
+  onFileChanged: ((file: File | null) => void);
 }
 
-class SelectFileRenderer extends React.Component<{}, ISelectFileState> {
-  constructor(props: {}) {
+class SelectFileRenderer extends React.Component<ISelectFileProps, {}> {
+  constructor(props: ISelectFileProps) {
     super(props);
     this.fileChanged = this.fileChanged.bind(this);
-    this.rankClick = this.rankClick.bind(this);
-    this.createItemArray = this.createItemArray.bind(this);
-    this.state = { selectedFile: null, items: Array<string>() };
   }
 
   public render() {
-    const rankButton =
-      this.state.selectedFile !== null ? (
-        <button onClick={this.rankClick}>Rank</button>
-      ) : (
-        ''
-      );
-
-    const rankerComponent = <RankerRenderer items={this.state.items} />;
-
-    if (this.state.items.length === 0) {
-      return (
-        <div>
-          <input
-            type="file"
-            accept=".txt"
-            multiple={false}
-            onChange={this.fileChanged}
-          />
-          {rankButton}
-        </div>
-      );
-    } else {
-      return rankerComponent;
-    }
+    return (
+      <div>
+        <input
+          type="file"
+          accept=".txt"
+          multiple={false}
+          onChange={this.fileChanged}
+        />
+      </div>
+    );
   }
 
   private fileChanged(event: React.ChangeEvent<HTMLInputElement>) {
     const files = event.currentTarget.files;
     if (files !== null && files.length === 1) {
-      const filesArray = Array.from(files);
-      this.setState({ selectedFile: filesArray[0] });
+      this.props.onFileChanged(files[0]);
     } else {
-      this.setState({ selectedFile: null });
+      this.props.onFileChanged(null);
     }
-  }
-
-  private rankClick() {
-    const fileReader = new FileReader();
-
-    fileReader.onload = () => {
-      const text = fileReader.result;
-      this.createItemArray(text as string);
-    };
-
-    fileReader.readAsText(this.state.selectedFile as File);
-  }
-
-  private createItemArray(content: string) {
-    let lines = content.split('\r');
-    lines = lines.map(line => line.replace(/\r?\n|\r/g, ''));
-    let items = this.state.items.splice(0);
-    items = items.concat(lines.splice(0));
-    this.setState({ items });
   }
 }
 
